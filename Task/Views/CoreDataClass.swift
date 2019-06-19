2155fcbc72347676754301a6b4a35d78a05ed9b8
Nic Gibson <nic@shpangy.com>
2019-06-19 15:11:20 -0600

    Started the view hierarchy as well as the Core Data Stack

---
+++ Task/Views/CoreDataClass.swift
@@ -0,0 +1,27 @@
+//
+//  CoreDataClass.swift
+//  Task
+//
+//  Created by Nic Gibson on 6/19/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
+        let container = NSPersistentContainer(name: appName)
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var managedObjectContext: NSManagedObjectContext { return container.viewContext }
+}

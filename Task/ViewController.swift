41e2db4a5c5c2b50a3235ba2119ac743a6536a6c
Nic Gibson <nic@shpangy.com>
2019-06-19 14:48:33 -0600

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Nic Gibson on 6/19/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
